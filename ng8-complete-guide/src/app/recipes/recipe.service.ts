3cfe536ced707641cde273ab26e11c615dbcc064
Alex <akuziakov@gmail.com>
2019-12-22 18:45:08 +0200

    feat(shoppingList): add recipe ingredients to shopping list.

--- ng8-complete-guide/src/app/recipes/recipe.service.ts
+++ ng8-complete-guide/src/app/recipes/recipe.service.ts
@@ -1,22 +1,33 @@
-import { EventEmitter } from '@angular/core';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { Ingredient } from './../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 
+@Injectable({ providedIn: 'root' })
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
-      'A First Test Recipe',
-      'This is a first simple test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'A second Test Recipe',
-      'This is a second simple test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'Big fat burger',
+      'What else you neet to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     )
   ];
   recipeSelected = new EventEmitter<Recipe>();
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- ng8-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ng8-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -19,6 +19,6 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngridient = new Ingredient(ingName, ingAmount);
-    this.shoppingListService.addItem(newIngridient);
+    this.shoppingListService.addIngredient(newIngridient);
   }
 }
