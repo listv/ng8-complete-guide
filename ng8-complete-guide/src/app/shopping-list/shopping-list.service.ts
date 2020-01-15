eeddc84b9e71ab87cb4964348f33bed87461474d
Oleksandr Kuziakov <oleksandr.kuziakov@ihsmarkit.com>
2020-01-15 12:26:26 +0200

    Replace EventEmitter with Subject.

--- ng8-complete-guide/src/app/shopping-list/shopping-list.service.ts
+++ ng8-complete-guide/src/app/shopping-list/shopping-list.service.ts
@@ -1,12 +1,12 @@
-import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   public getIngredients() {
     return this.ingredients.slice();
@@ -14,11 +14,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients);
+    this.ingredientsChanged.next(this.ingredients);
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
